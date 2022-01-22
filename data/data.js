a297fd58255f0b36898d6eb12b8e37d5f3f9dd99
ratigumashvili <ratigumashvili@gmail.com>
2022-01-22 14:07:22 +0400

    Added src files

---
+++ data/data.js
@@ -0,0 +1,212 @@
+const data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 0,
+            "placename": "0",
+            "region": "აფხაზეთი",
+            "category": "ლიტერატურა",
+            "author": "",
+            "textTitle": "",
+            "textFragment": "",
+            "textType": "",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          41.31134033203125,
+          42.78733853171998
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 1,
+            "placename": "1",
+            "region": "აფხაზეთი",
+            "category": "ისტორია",
+            "author": "",
+            "textTitle": "",
+            "textFragment": "",
+            "textType": "",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996-01-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          41.351165771484375,
+          42.775243380699706
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 2,
+            "placename": "2",
+            "region": "აფხაზეთი",
+            "category": "ლიტერატურა",
+            "author": "",
+            "textTitle": "",
+            "textFragment": "",
+            "textType": "",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996-01-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          41.167144775390625,
+          42.809506838324204
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 3,
+            "placename": "3",
+            "region": "აფხაზეთი",
+            "category": "ისტორია",
+            "author": "",
+            "textTitle": "",
+            "textFragment": "",
+            "textType": "",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996-01-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          41.472015380859375,
+          42.71977704089522
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 4,
+            "placename": "4",
+            "region": "აფხაზეთი",
+            "category": "ლიტერატურა",
+            "author": "",
+            "textTitle": "",
+            "textFragment": "",
+            "textType": "",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996-01-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          41.47072792053223,
+          42.71157894243348
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 5,
+            "placename": "5",
+            "region": "აფხაზეთი",
+            "category": "ისტორია",
+            "author": "",
+            "textTitle": "",
+            "textFragment": "",
+            "textType": "",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996-01-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          41.46068572998047,
+          42.714164613485046
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 6,
+            "placename": "6",
+            "region": "აფხაზეთი",
+            "category": "ლიტერატურა",
+            "author": "",
+            "textTitle": "",
+            "textFragment": "",
+            "textType": "",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996-01-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          40.97900390625,
+          43.004647127794435
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 7,
+            "placename": "7",
+            "region": "აფხაზეთი",
+            "category": "ლიტერატურა",
+            "author": "",
+            "textTitle": "",
+            "textFragment": "",
+            "textType": "",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996-01-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          41.0394287109375,
+          43.01067254527255
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+            "recordId": 8,
+            "placename": "ეშერა",
+            "region": "აფხაზეთი",
+            "category": "ლიტერატურა",
+            "author": "გრიგოლ ხანძთელი",
+            "textTitle": "აბოს წამება (წამებაი წმიდისა და ნეტარისა მოწამისა ქრისტეისისა ჰაბოისი, რომელი იწამა ქართლსა შინა, ქალაქსა ტფილისს, ხელითა სარკინოზთაითა, გამოთქმული იოვანე, ძისა საბანისი. ბრძანებითა ქრისტეის მიერ სამოელ ქართლის კათალიკოზისაითა)",
+            "textFragment": "",
+            "textType": "წამებათა ჟანრის ჰაგიოგრაფიული თხზულება",
+            "textDate": 1995,
+            "start": "1994",
+            "end": "1996-01-01"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          40.93231201171875,
+          43.04681263770761
+        ]
+      }
+    }
+  ]
+}
---
+++ data/recordtable.js
@@ -0,0 +1,103 @@
+var recordData = {
+    records: [{
+        year: 21,
+        name: "Wells Ferrell",
+        gender: "male",
+        email: "wellsferrell@limozen.com"
+    }, {
+        year: 22,
+        name: "Burton Pena",
+        gender: "male",
+        email: "burtonpena@limozen.com"
+    }, {
+        year: 22,
+        name: "Buchanan Nelson",
+        gender: "male",
+        email: "buchanannelson@limozen.com"
+    }, {
+        year: 27,
+        name: "Marshall Armstrong",
+        gender: "male",
+        email: "marshallarmstrong@limozen.com"
+    }, {
+        year: 21,
+        name: "Sofia Dixon",
+        gender: "female",
+        email: "sofiadixon@limozen.com"
+    }, {
+        year: 38,
+        name: "Kent Mason",
+        gender: "male",
+        email: "kentmason@limozen.com"
+    }, {
+        year: 21,
+        name: "Cummings Rodgers",
+        gender: "male",
+        email: "cummingsrodgers@limozen.com"
+    }, {
+        year: 27,
+        name: "Lorrie Haynes",
+        gender: "female",
+        email: "lorriehaynes@limozen.com"
+    }, {
+        year: 27,
+        name: "Lisa Sampson",
+        gender: "female",
+        email: "lisasampson@limozen.com"
+    }, {
+        year: 38,
+        name: "Anita Perkins",
+        gender: "female",
+        email: "anitaperkins@limozen.com"
+    }, {
+        year: 38,
+        name: "Barr Jacobs",
+        gender: "male",
+        email: "barrjacobs@limozen.com"
+    }, {
+        year: 34,
+        name: "Clayton Best",
+        gender: "male",
+        email: "claytonbest@limozen.com"
+    }, {
+        year: 36,
+        name: "Craig Blake",
+        gender: "male",
+        email: "craigblake@limozen.com"
+    }, {
+        year: 39,
+        name: "Marisol Maldonado",
+        gender: "female",
+        email: "marisolmaldonado@limozen.com"
+    }, {
+        year: 20,
+        name: "Rene Odonnell",
+        gender: "female",
+        email: "reneodonnell@limozen.com"
+    }, {
+        year: 25,
+        name: "Melanie Oconnor",
+        gender: "female",
+        email: "melanieoconnor@limozen.com"
+    }, {
+        year: 25,
+        name: "Odom Peterson",
+        gender: "male",
+        email: "odompeterson@limozen.com"
+    }, {
+        year: 32,
+        name: "Wilder Johnston",
+        gender: "male",
+        email: "wilderjohnston@limozen.com"
+    }, {
+        year: 28,
+        name: "Lavonne Head",
+        gender: "female",
+        email: "lavonnehead@limozen.com"
+    }, {
+        year: 39,
+        name: "Shelby Gardner",
+        gender: "female",
+        email: "shelbygardner@limozen.com"
+    }]
+};
